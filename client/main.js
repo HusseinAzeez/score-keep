b84093184b7fa58154b1729a56d95eba855c75a1
HusseinAzeez <hussein.azeez91@gmail.com>
2018-08-12 01:49:46 +0700

    Add updating the score buttons

--- client/main.js
+++ client/main.js
@@ -9,7 +9,12 @@ const renderPlayers = (players) => {
     return (
       <p key={player._id}>
         {player.name} has {player.score} point(s).
-        <button onClick={() => Players.remove({_id: player._id})}>X</button>
+        <button onClick={() => {
+          Players.update(player._id, {$inc: {score: -1}});}}>-1</button>
+        <button onClick={() => {
+          Players.update(player._id, {$inc: {score: 1}});}}>+1</button>
+        <button onClick={() => { 
+          Players.remove({_id: player._id});}}>X</button>
       </p>
     );
   });
