93d670bc8014a3cb0bb801268362e833ccf07d3a
HusseinAzeez <hussein.azeez91@gmail.com>
2018-08-13 22:43:24 +0700

    Add app component

--- client/main.js
+++ client/main.js
@@ -2,51 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
 import {Tracker} from 'meteor/tracker';
-import {Players} from './../imports/api/players';
-
-const renderPlayers = (players) => {
-  return players.map((player) => {
-    return (
-      <p key={player._id}>
-        {player.name} has {player.score} point(s).
-        <button onClick={() => {
-          Players.update(player._id, {$inc: {score: -1}});}}>-1</button>
-        <button onClick={() => {
-          Players.update(player._id, {$inc: {score: 1}});}}>+1</button>
-        <button onClick={() => { 
-          Players.remove({_id: player._id});}}>X</button>
-      </p>
-    );
-  });
-};
-const addPlayer = (event) => {
-  let name = event.target.playerName.value;
-  event.preventDefault();
-  if (name) {
-    event.target.playerName.value = '';
-    Players.insert({
-      name: name,
-      score: 15
-    });
-  }
-};
+import {Players} from '../imports/api/players';
+import App from '../imports/ui/App';
 
 Meteor.startup(() => {
   Tracker.autorun(() => {
-    let players = Players.find().fetch();
+    let players = Players.find({}, {sort: {score: -1}}).fetch();
     let title = 'Score Keep';
-    let name = 'Xizors';
-    let jsx = (
-      <div>
-        <h1>{title}</h1>
-        <p>Hello {name}!</p>
-        {renderPlayers(players)}
-        <form onSubmit={addPlayer}>
-          <input type="text" name="playerName" placeholder="Player Name"/>
-          <button>Add Player</button>
-        </form>
-      </div>
-    );
-    ReactDOM.render(jsx, document.getElementById('app'));
+    ReactDOM.render(<App players={players} title={title} />, document.getElementById('app'));
   });
 });
---
+++ imports/ui/AddPlayer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Players } from '../api/players';
+
+export default class AddPlayer extends React.Component {
+    addPlayer(event) {
+      let playerName = event.target.playerName.value;
+      event.preventDefault();
+      if (playerName) {
+        event.target.playerName.value = '';
+        Players.insert({
+          name: playerName,
+          score: 0,
+        });
+      }
+    }
+    
+    render() {
+        return (
+          <div>
+            <form onSubmit={this.addPlayer.bind(this)}>
+              <input type="text" name="playerName" placeholder="Player name"/>
+              <button>Add Player</button>
+            </form>
+          </div>
+        );
+    }
+}
